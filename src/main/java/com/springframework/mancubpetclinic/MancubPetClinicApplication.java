3f6e20e1365e7788c7100dcc955a155fea08d1c4
Abiodun Giwa <biodun1230@gmail.com>
2021-01-31 18:45:44 -0500

    initial project add

---
+++ src/main/java/com/springframework/mancubpetclinic/MancubPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.mancubpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancubPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancubPetClinicApplication.class, args);
+	}
+
+}
